154d8f053d26d53ff46347af6ad3797650869394
Raul Martin Catalan <rmartinc96@gmail.com>
2021-12-01 09:29:14 +0100

    Token JWT

---
+++ src/main/java/com/example/enjoypadelapi/model/JwtRequest.java
@@ -0,0 +1,14 @@
+package com.example.enjoypadelapi.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtRequest {
+    private String username;
+    private String password;
+}
